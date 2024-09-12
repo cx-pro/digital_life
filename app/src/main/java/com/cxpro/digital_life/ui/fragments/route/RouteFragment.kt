85204894600fac5871ca56094a54da8fad9e6a57
CXI12 <160449692+cx-pro@users.noreply.github.com>
2024-09-12 16:08:13 +0800

    202409121608

--- app/src/main/java/com/cxpro/digital_life/ui/fragments/route/RouteFragment.kt
+++ app/src/main/java/com/cxpro/digital_life/ui/fragments/route/RouteFragment.kt
@@ -1,9 +1,12 @@
 package com.cxpro.digital_life.ui.fragments.route
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.location.Location
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,29 +16,41 @@ import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.cxpro.digital_life.R
 import com.cxpro.digital_life.databinding.FragmentRouteBinding
 import com.cxpro.digital_life.utils.PermissionManager
 import com.google.android.gms.location.CurrentLocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.osmdroid.bonuspack.routing.OSRMRoadManager
 import org.osmdroid.bonuspack.routing.Road
 import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.MapEventsOverlay
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Polyline
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.IMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
-const val USER_AGENT="DigitalLifeAgent/1.0"
+const val USER_AGENT = "DigitalLifeAgent/1.0"
+
+var lastRoadFetchingJob: Job? = null
+var lastEndMarker: Marker? = null
+var lastEndPoint: GeoPoint? = null
+var lastStartPoint: GeoPoint? = null
+var beforeLastEndPoint: GeoPoint? = null
+var lastRoute: Polyline? = null
 
 class RouteFragment : Fragment() {
 
@@ -43,7 +58,6 @@ class RouteFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var factory: RouteViewModel.Factory
     private val viewModel by viewModels<RouteViewModel>()
-    private lateinit var map: MapView
 
     @SuppressLint("InflateParams", "SetJavaScriptEnabled")
     override fun onCreateView(
@@ -51,10 +65,8 @@ class RouteFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentRouteBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        map = binding.map
         Configuration.getInstance().userAgentValue = USER_AGENT
         val activity=requireActivity()
         if(!PermissionManager.checkOrGetLocationPermission(activity)){
@@ -71,47 +83,38 @@ class RouteFragment : Fragment() {
             activity.findNavController(R.id.nav_host_fragment_activity_main)
                 .navigate(R.id.navigation_home)
         }
-        LocationServices.getFusedLocationProviderClient(activity).getCurrentLocation(
-            CurrentLocationRequest.Builder()
-                .setDurationMillis(30000).build(),
-            null
-        ).addOnSuccessListener {
-            val startPoint = GeoPoint(23.716920, 120.528952)
-            val endPoint = GeoPoint(23.692862, 120.538184)
-            map.setExpectedCenter(startPoint)
-            val startMarker = Marker(map)
-            val endMarker = Marker(map)
-
-            startMarker.position = startPoint
-            endMarker.position = endPoint
-            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-            endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-
-            map.overlays.add(startMarker)
-            map.overlays.add(endMarker)
-            CoroutineScope(Dispatchers.IO).launch {
-                val roadManager = OSRMRoadManager(context, USER_AGENT)
-                roadManager.setMean(OSRMRoadManager.MEAN_BY_CAR)
-
-                fetchingRouteMessage(context)
-                var road=roadManager.getRoad(arrayListOf(startPoint,endPoint))
-
-                while(road.mStatus!=Road.STATUS_OK) {
-                    fetchingRouteMessage(context)
-                    road = roadManager.getRoad(arrayListOf(startPoint, endPoint))
+        val mLocationOverlay =
+            object : MyLocationNewOverlay(GpsMyLocationProvider(context), binding.map) {
+                override fun onLocationChanged(location: Location?, source: IMyLocationProvider?) {
+                    super.onLocationChanged(location, source)
+                    location?.let {
+                        val startPoint = GeoPoint(it)
+                        if(lastEndPoint != null && startPoint != lastStartPoint)
+                            fetchBestRoute(context, binding.map, startPoint, lastEndPoint)
+                        beforeLastEndPoint = lastEndPoint
+                        lastStartPoint = startPoint
+                    }
                 }
-                map.overlays.add(RoadManager.buildRoadOverlay(road))
             }
+        mLocationOverlay.enableFollowLocation()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            if(Looper.myLooper() == null)Looper.prepare()
+            mLocationOverlay.enableMyLocation()
         }
+        binding.map.setTileSource(TileSourceFactory.MAPNIK)
+        fetchCurrentLocationAndPerform(activity).addOnSuccessListener {
+            val startPoint=GeoPoint(it)
+            val mapEventsOverlay = MapEventsOverlay(MapEventsReceiverImpl(context, startPoint, binding.map))
+            binding.map.overlays.add(mapEventsOverlay)
+            binding.map.setExpectedCenter(startPoint)
+        }
+        binding.map.setMultiTouchControls(true)
+        binding.map.setZoomLevel(20.0)
 
-        map.setTileSource(TileSourceFactory.MAPNIK)
-        map.setMultiTouchControls(true)
-        map.setZoomLevel(18.0)
 
-        val mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), map)
-        mLocationOverlay.enableMyLocation()
-        map.overlays.add(mLocationOverlay)
 
+        binding.map.overlays.add(mLocationOverlay)
         return root
     }
 
@@ -123,7 +126,7 @@ class RouteFragment : Fragment() {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this)
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-        map.onResume() //needed for compass, my location overlays, v6.0.0 and up
+        binding.map.onResume() //needed for compass, my location overlays, v6.0.0 and up
     }
 
     override fun onPause() {
@@ -132,12 +135,63 @@ class RouteFragment : Fragment() {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this)
         //Configuration.getInstance().save(this, prefs)
-        map.onPause() //needed for compass, my location overlays, v6.0.0 and up
+        binding.map.onPause() //needed for compass, my location overlays, v6.0.0 and up
     }
 }
-fun fetchingRouteMessage(context: Context?){
-    CoroutineScope(Dispatchers.IO).launch {
+
+
+fun fetchingRouteMessage(context: Context?): Job {
+    return CoroutineScope(Dispatchers.IO).launch {
         if(Looper.myLooper() == null)Looper.prepare()
-        Toast.makeText(context, R.string.loading_route_msg, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, R.string.loading_route_msg, Toast.LENGTH_LONG).show()
     }
+}
+
+@SuppressLint("MissingPermission")
+fun fetchCurrentLocationAndPerform(activity: Activity): Task<Location> {
+    return (
+            LocationServices
+                .getFusedLocationProviderClient(activity)
+                .getCurrentLocation(
+                    CurrentLocationRequest.Builder()
+                        .setDurationMillis(30000).build()
+                    ,null
+                )
+            )
+}
+
+class MapEventsReceiverImpl(private val context: Context?, private val startPoint: GeoPoint?, private val map: MapView) : MapEventsReceiver {
+
+    override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
+        return true
+    }
+
+    override fun longPressHelper(endPoint: GeoPoint?): Boolean {
+        if (lastEndMarker != null) map.overlays.remove(lastEndMarker)
+        lastEndPoint = endPoint
+        val endMarker = Marker(map)
+        endMarker.position = endPoint
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map.overlays.add(endMarker)
+        lastEndMarker = endMarker
+        fetchBestRoute(context, map, startPoint, endPoint)
+        return false
+    }
+}
+
+fun fetchBestRoute(context: Context?, map: MapView, startPoint: GeoPoint?, endPoint: GeoPoint?){
+
+    lastRoadFetchingJob?.cancel()
+    lastRoadFetchingJob = (CoroutineScope(Dispatchers.IO).launch {
+        val roadManager = OSRMRoadManager(context, USER_AGENT)
+        roadManager.setMean(OSRMRoadManager.MEAN_BY_CAR)
+        fetchingRouteMessage(context)
+        var road=roadManager.getRoad(arrayListOf(startPoint,endPoint))
+
+        while(road.mStatus!=Road.STATUS_OK)
+            road = roadManager.getRoad(arrayListOf(startPoint, endPoint))
+        if (lastRoute != null) map.overlays.remove(lastRoute)
+        lastRoute = RoadManager.buildRoadOverlay(road)
+        map.overlays.add(lastRoute)
+    })
 }
